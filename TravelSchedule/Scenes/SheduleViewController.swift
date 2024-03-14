0b9066395305e3343c58303e6227b808b2cf0ab3
Alexander Ulianovskiy <a.ulianovskiy@yandex.ru>
2024-03-14 08:57:59 +0300

    stations response successful

--- TravelSchedule/Scenes/SheduleViewController.swift
+++ TravelSchedule/Scenes/SheduleViewController.swift
@@ -125,10 +125,29 @@ final class SheduleViewController: UIViewController {
     private func stationsList() {
         guard let service = create(service: .stationsList) as? StationsListService else { return }
         Task {
-            let stations = try await service.getListOfAllStations()
-            print(stations)
-            label.text = "\(stations)"
-            uiBlockingProgressHUD?.dismissCustom()
+            do {
+                let response = try await service.getListOfAllStations()
+                let countries = response.countries
+                label.text = String(
+                    """
+                    Countries count \(countries?.count)
+                    """
+                )
+                uiBlockingProgressHUD?.dismissCustom()
+                } catch let DecodingError.dataCorrupted(context) {
+                    print(context)
+                } catch let DecodingError.keyNotFound(key, context) {
+                    print("Key '\(key)' not found:", context.debugDescription)
+                    print("codingPath:", context.codingPath)
+                } catch let DecodingError.valueNotFound(value, context) {
+                    print("Value '\(value)' not found:", context.debugDescription)
+                    print("codingPath:", context.codingPath)
+                } catch let DecodingError.typeMismatch(type, context)  {
+                    print("Type '\(type)' mismatch:", context.debugDescription)
+                    print("codingPath:", context.codingPath)
+                } catch {
+                    print("❌ ERROR: \(error.localizedDescription)")
+                }
         }
     }
 }
