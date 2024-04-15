45b8f15082d856c8d3ac51692849c1a130a9df67
Alexander Ognerubov <a.ulianovskiy@yandex.ru>
2024-04-15 22:04:13 +0300

    webview added

--- TravelSchedule/Views/SettingsView.swift
+++ TravelSchedule/Views/SettingsView.swift
@@ -11,6 +11,8 @@ struct SettingsView: View {
     
     @State var isDarkModeOn: Bool = false
     @Binding var appearanceSelection: Int
+    @StateObject var viewModel = WebView.ProgressViewModel(progress: 0.0)
+    @State var isProgressViewVisible: Bool = true
     
     var body: some View {
         VStack {
@@ -22,12 +24,30 @@ struct SettingsView: View {
             .onChange(of: isDarkModeOn) { value in
                 value ? (appearanceSelection = 2) : (appearanceSelection = 1)
             }
-            HStack {
-                Text("User agreement")
-                Spacer()
-                Image(uiImage: UIImage.arrowRight)
-                    .tint(Color.init(UIColor.blackDay))
-            }
+            NavigationLink(destination: {
+                VStack {
+                    ProgressView(value: viewModel.progress)
+                        .progressViewStyle(.linear)
+                        .opacity(isProgressViewVisible ? 1 : 0)
+                    WebView(url: URL(string: "https://yandex.ru/legal/practicum_offer/") ?? URL(string: "")!, viewModel: viewModel)
+                }
+                .padding(.top, 88)
+                .ignoresSafeArea()
+                .navigationTitle("User agreement")
+                .toolbarRole(.editor)
+                .onChange(of: viewModel.progress, perform: { value in
+                    if value > 0.99 {
+                        isProgressViewVisible = false
+                    }
+                })
+            }, label: {
+                HStack {
+                    Text("User agreement")
+                    Spacer()
+                    Image(uiImage: UIImage.arrowRight)
+                        .tint(Color.init(UIColor.blackDay))
+                }
+            })
             .frame(height: 60)
             Spacer()
             CustomTextView(string: "The application uses the Yandex.Schedules API \n Version 1.0 (beta)", size: 12, weight: .regular, color: UIColor.blackDay)
